4db0dae2cdbde12722b4107514dc81d0ed38fdda
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2021-10-10 23:53:23 +0900

    Add Activities.vue

---
+++ src/components/Activities.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid fill-height>
+    <v-row justify="center">
+      <p class="text-center text-h4">Activities</p>
+    </v-row>
+    <v-row justify="center">
+      <p class="body-1">
+        月に1回程度東京都内にて
+        <br />
+        コートを借りて練習します！
+        <br />
+        人数に合わせて活動回数も
+        <br />
+        増やしていく予定です。
+        <br />
+        参加希望者や気になった方は
+        <br />
+        公式TwitterにてDMで問い合わせください！
+      </p>
+      
+    </v-row>
+    <Tweet id="783943172057694208"></Tweet>
+
+  </v-container>
+</template>
+<script>
+import Tweet from 'vue-tweet-embed'
+export default {
+  name: "Activities",
+   component: {
+       Tweet,
+   },
+   data() {
+    return {
+      twitterId: "c8w5y",
+    }
+   }
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,17 +2,21 @@
   <section class="home">
     <MainImage></MainImage>
     <Concept></Concept>
+    <Activities></Activities>
   </section>
 </template>
 
 <script>
 import MainImage from "../components/MainImage.vue"
 import Concept from "../components/Consept.vue";
+import Activities from "../components/Activities.vue"
+
 export default {
   name: "Home",
   components: {
     Concept,
     MainImage,
+    Activities,
   },
 };
 </script>
